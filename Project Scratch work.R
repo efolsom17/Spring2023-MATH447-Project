13dd1fa4441a27b108c6237a6b419590c8317605
efolsom17 <eric.folsom17@gmail.com>
2023-05-10 14:48:48 -0700

    Cleaned data is beer_by_name. Average review by name with beer style attached

--- Project Scratch work.R
+++ Project Scratch work.R
@@ -20,24 +20,29 @@ beer2 <- beer2 %>%
   group_by(beer_name) %>%
   fill(beer_abv, .direction = "downup")
 sum(is.na(beer2$beer_abv))
-
-
+map2<-select(beer_map, beer_name, beer_style)
+map2<-map2%>%distinct(beer_name,.keep_all = TRUE)
 
 #cleaning data
 beer_by_name<-beer2%>%
   group_by(beer_name)%>%
   summarise(across(c(review_overall, review_aroma, review_appearance, review_palate, review_taste, beer_abv), mean))%>%
   filter(!is.na(beer_abv))
+
+beer_by_name<-left_join(beer_by_name, map2, by="beer_name")
 beer_by_name<-column_to_rownames(beer_by_name,var='beer_name')
-beer_by_style<-beer2%>%
-  group_by(beer_style)%>%
-  summarise(across(c(review_overall, review_aroma, review_appearance, review_palate, review_taste, beer_abv), mean))%>%
-  filter(!is.na(beer_abv))
-beer_by_style<-column_to_rownames(beer_by_style,var='beer_style')
+
 View(beer_by_name)
 length(beer_by_name$beer_abv)
-View(beer_by_style)
-length(beer_by_style$beer_abv)
+
+
+#beer_by_style<-beer2%>%
+#  group_by(beer_style)%>%
+#  summarise(across(c(review_overall, review_aroma, review_appearance, review_palate, review_taste, beer_abv), mean))%>%
+#  filter(!is.na(beer_abv))
+#beer_by_style<-column_to_rownames(beer_by_style,var='beer_style')
+#View(beer_by_style)
+#length(beer_by_style$beer_abv)
 
 #Exploratory Data Analysis
 
@@ -50,3 +55,6 @@ pc.beer_by_name<-prcomp(beer_by_name,scale=TRUE)
 
 
 #model creation
+
+
+
