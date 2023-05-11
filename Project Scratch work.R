bc83236bd1e7190de73bc3619029223e609e68ce
efolsom17 <eric.folsom17@gmail.com>
2023-05-10 20:59:48 -0700

    got PCA to work, and made some fun lists

--- Project Scratch work.R
+++ Project Scratch work.R
@@ -33,16 +33,16 @@ head(beer)
 #We will remove some of the non-important variables, but we may need them for later
 beer_map<-select(beer, brewery_id,brewery_name, beer_name, beer_beerid, beer_style, beer_abv)
 #removing some of the unnecessary columns in the dataset
-beer2<-select(beer,-review_profilename, -brewery_name, -brewery_id,-beer_beerid, -review_time)
+beer.clean<-select(beer,-review_profilename, -brewery_name, -brewery_id,-beer_beerid, -review_time)
 #a lot of abv values were missing so we need to take care of those
 #filling in the missing values that we can from other reviews of the same beer
-beer2 <- beer2 %>%
+beer.clean <- beer.clean %>%
   group_by(beer_name) %>%
   fill(beer_abv, .direction = "downup")
 #removing the rows with missing values which we couldn't fill in
-beer2<-beer2%>%
+beer.clean<-beer.clean%>%
   filter(!is.na(beer_abv))
-#now beer2 is clean raw data if we need to use it later
+#now beer.clean is clean raw data if we need to use it later
 
 
 #this will be useful later
@@ -52,7 +52,7 @@ map2<-map2%>%distinct(beer_name,.keep_all = TRUE)
 ##condensing the data down to what we want
 
 #taking the average review of each beer for each category
-beer_avg<-beer2%>%
+beer_avg<-beer.clean%>%
   group_by(beer_name)%>%
   summarise(across(c(review_overall, review_aroma, review_appearance, review_palate, review_taste, beer_abv), mean))
 #adding the style of each beer
@@ -60,7 +60,7 @@ beer_avg<-left_join(beer_avg, map2, by="beer_name")
 beer_avg<-as_tibble(beer_avg)
 
 #getting the variance of each review
-beer_var<-beer2%>%
+beer_var<-beer.clean%>%
   group_by(beer_name)%>%
   summarise(across(c(review_overall, review_aroma, review_appearance, review_palate, review_taste), var))
 #for the beers where there were only one review, there is 0 variance,
@@ -73,7 +73,7 @@ colnames.new.var<-c( review_overall_Var="review_overall",review_aroma_Var="revie
 beer_var<-rename(beer_var, all_of(colnames.new.var))
 
 #calcualting the number of reviews for each beer
-beer_n<-beer2%>%
+beer_n<-beer.clean%>%
   group_by(beer_name)%>%
   summarise(n=n())
 
@@ -89,31 +89,35 @@ beer_avg<-beer_avg%>%
     review_aroma_Var, review_appearance, review_appearance_Var, review_palate, 
     review_palate_Var,  review_taste, review_taste_Var)
 
-#finalizing our clean, useable data
 
-beer.clean<-beer_avg
-
-head(beer.clean)
-View(beer.clean)
+head(beer_avg)
+View(beer_avg)
 
 #need this for PCA
-beer.clean.df<-column_to_rownames(beer.clean,var="beer_name")
-head(beer.clean.df)
+beer_avg.df<-column_to_rownames(beer_avg,var="beer_name")
+head(beer_avg.df)
 
 #need this for something else I think. If we want to use some form of logistic regression
-beer.clean.fac_style<-beer.clean.df%>%mutate(beer_style=as.factor(beer_style))
-head(beer.clean.fac_style)
+beer_avg.fac_style<-beer_avg.df%>%mutate(beer_style=as.factor(beer_style))
+head(beer_avg.fac_style)
 
+#List object, each item in the list corresponds to the style of beer
+beer.avg_list<-split(beer_avg, beer_avg$beer_style)
+View(beer.avg_list)
+
+#raw list of the data, broken down by beer style
+beer.raw_list<-split(beer.clean, beer.clean$beer_style)
+View(beer.raw_list)
 #cleaning up the R environment
-rm(beer_n,beer_var,beer_avg, colnames.new.var)
+rm(beer_n,beer_var, colnames.new.var)
 
 #un-comment if you want to save a .csv of the data
-#write_csv(beer.clean, "beer_reviews_average.csv")
-#write_csv(beer2, "beer_reviews_clean.csv")
+#write_csv(beer_avg, "beer_reviews_average.csv")
+#write_csv(beer.clean, "beer_reviews_clean.csv")
 
 #stuff we probably won't use, but if we need it just uncomment the code
 #View(beer_var)
-#beer_by_style<-beer2%>%
+#beer_by_style<-beer.clean%>%
 #  group_by(beer_style)%>%
 #  summarise(across(c(review_overall, review_aroma, review_appearance, review_palate, review_taste, beer_abv), mean))%>%
 #  filter(!is.na(beer_abv))
@@ -122,40 +126,39 @@ rm(beer_n,beer_var,beer_avg, colnames.new.var)
 #length(beer_by_style$beer_abv)
 
 
-
 ###################
 #Exploratory Data Analysis####
 
 #making life a little easier for now
-attach(beer.clean)
+attach(beer_avg)
 
 #not gonna comment this till we have something concrete
 
-styles<-distinct(beer.clean, beer_style)
+styles<-distinct(beer_avg, beer_style)
 
 #pairs plot, kinda cool looking, WARNING: will turn your macbook into a fighter-jet
-ggpairs(beer.clean, columns=c(3,4,5,7,9,11,13),aes(color = beer_style, alpha = 0.5))
+ggpairs(beer_avg, columns=c(3,4,5,7,9,11,13),aes(color = beer_style, alpha = 0.5))
 
 #I think we need to remove some outliers from the abv
 
 #get ready for a lot of scatter plots, mostly comparing with overall review
 
-abv.plot<-ggplot(beer.clean, aes(beer_abv,review_overall, color=beer_style))
-aroma.plot<-ggplot(beer.clean, aes(review_aroma,review_overall, color=beer_style))
-appearance.plot<-ggplot(beer.clean, aes(review_appearance,review_overall, color=beer_style))
-palate.plot<-ggplot(beer.clean, aes(review_palate,review_overall, color=beer_style))
-taste.plot<-ggplot(beer.clean, aes(review_taste,review_overall, color=beer_style))
+abv.plot<-ggplot(beer_avg, aes(beer_abv,review_overall, color=beer_style))
+aroma.plot<-ggplot(beer_avg, aes(review_aroma,review_overall, color=beer_style))
+appearance.plot<-ggplot(beer_avg, aes(review_appearance,review_overall, color=beer_style))
+palate.plot<-ggplot(beer_avg, aes(review_palate,review_overall, color=beer_style))
+taste.plot<-ggplot(beer_avg, aes(review_taste,review_overall, color=beer_style))
 
 #abv vs overall review
 
 abv.plot+geom_point()+geom_smooth(method = lm, se = FALSE)+
   theme(legend.position = "none")
-abv.plot.ungrouped<-ggplot(beer.clean, aes(beer_abv,review_overall))
+abv.plot.ungrouped<-ggplot(beer_avg, aes(beer_abv,review_overall))
 abv.plot.ungrouped+geom_point()+geom_smooth()+
   theme(legend.position = "none")
 
 #R really doesn't want to make a heatmap, this will be figured out later
-heatmap_data <- beer.clean[c('review_overall', 'review_aroma', 'review_appearance', 'review_palate', 'review_taste')]
+heatmap_data <- beer_avg[c('review_overall', 'review_aroma', 'review_appearance', 'review_palate', 'review_taste')]
 
 heatmap_data<-as.matrix(heatmap_data)
 heatmap(heatmap_data)
@@ -177,15 +180,25 @@ ggplot(data = heatmap_data_melt, aes(x = names, y = name, fill = value)) +
        x="",
        y="")
 
-ggplot(beer2, aes(beer_abv, review_overall))+geom_point()
-abv.dot<-ggplot(beer2, aes(beer_abv))
-abv.scat<-ggplot(beer2, aes(beer_beerid, beer_abv))
+
+#testing some scatter plot stuff
+ggplot(beer.clean, aes(beer_abv, review_overall))+geom_point()
+abv.dot<-ggplot(beer.clean, aes(beer_abv))
+abv.scat<-ggplot(beer.clean, aes(beer_beerid, beer_abv))
 abv.scat+geom_point()
 abv.dot+geom_dotplot()
-pc.beer_by_name<-prcomp(beer_by_name,scale=TRUE)
 
-
-detach(beer.clean)
+pc.beer<-prcomp(beer_avg.fac_style[,-1], scale=TRUE)
+pc.beer$rotation
+biplot(pc.beer, scale=0)
+pc.beer.var<-pc.beer$sdev^2
+pve.beer<-pc.beer.var/sum(pc.beer.var)
+temp.x<-c(1:length(pve.beer))
+pve.df<-data.frame(pve.beer,temp.x)
+scree.beer<-ggplot(pve.df, aes(x=temp.x, y=pve.beer))
+scree.beer+geom_point()+geom_line()
+temp.x
+detach(beer_avg)
 
 ####model creation####
 
@@ -194,14 +207,14 @@ detach(beer.clean)
 
 #80-20 split for training and testing data
 set.seed(42069)
-train<-sample(c(1:nrow(beer.clean)),floor(0.80 * nrow(beer.clean)))
+train<-sample(c(1:nrow(beer_avg)),floor(0.80 * nrow(beer_avg)))
 test<- -train
-beer.train<-beer.clean[train,]
-beer.test<-beer.clean[test,]
+beer.train<-beer_avg[train,]
+beer.test<-beer_avg[test,]
 
 #50/50 splot for training and testing data
 set.seed(42069)
-train<-sample(c(1:nrow(beer.clean)),floor(nrow(beer.clean)/2))
+train<-sample(c(1:nrow(beer_avg)),floor(nrow(beer_avg)/2))
 test<- -train
-beer.train<-beer.clean[train,]
-beer.test<-beer.clean[test,]
+beer.train<-beer_avg[train,]
+beer.test<-beer_avg[test,]
